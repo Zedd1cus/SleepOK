cba3569824481f886f9c9f65342b3ed365193eff
ninza <ninza147258@gmail.com>
2022-05-24 19:25:58 +0300

    started to handle_all_players and /start with only far if

--- handlers/basehandlers/start/start.py
+++ handlers/basehandlers/start/start.py
@@ -4,12 +4,17 @@ from aiogram import Dispatcher
 from keyboards.client_kb import verify_kb_scenario
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
+from database import connect
+from database.user import User
+from database.state_change import StateChange
+from database.advice import Advice
 
 class StartFMS(StatesGroup):
     start_state = State()
 
 
 async def command_start(message: types.Message):
+    user = None
     if True: # нижнее нужно проделывать только если юзера нет в бд, иначе - pass
         await bot.send_message(message.chat.id, "What's up!", reply_markup=verify_kb_scenario)
         await StartFMS.start_state.set()
